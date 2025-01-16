ce653c092974b7442c5f083507233f73e81a0f35
AoTuTap <thinhnpse98@gmail.com>
2025-01-17 00:07:57 +0200

    Pussh video 18 Setup React Router

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,8 +1,13 @@
+import { Outlet } from "react-router-dom"
+import AppHeader from "./components/layout/app.header"
 
 function Layout() {
 
   return (
-    <div>Hello world Phuc Thinh</div>
+    <div>
+      <AppHeader />
+      <Outlet />
+    </div>
   )
 }
 
